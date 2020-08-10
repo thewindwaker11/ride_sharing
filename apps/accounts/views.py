76dc56c27860795c21b73ad22d66550f3689291f
Kostis Bourlas <kostisbourlas@protonmail.com>
2020-08-10 15:14:01 +0300

    Add user login and register API

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from apps.accounts.serializers import UserSerializer, LogInSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LogInView(TokenObtainPairView):
+    serializer_class = LogInSerializer
--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.accounts.views import SignUpView, LogInView
+from rest_framework_simplejwt.views import TokenRefreshView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/sign_up', SignUpView.as_view(), name='sign_up'),
+    path('api/log_in', LogInView.as_view(), name='log_in'),
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
